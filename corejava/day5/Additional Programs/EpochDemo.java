5908777df9756a87f8ec9b7d9c422fcb93535e12
HTC-AP-Hadoop-Training <36527618+HTC-AP-Hadoop-Training@users.noreply.github.com>
2018-02-25 12:51:51 +0530

    Update EpochDemo.java

--- corejava/day5/Additional Programs/EpochDemo.java
+++ corejava/day5/Additional Programs/EpochDemo.java
@@ -1,29 +1,31 @@
 package com.htc.corejava.day5;
 
 import java.sql.Timestamp;
+import java.util.Calendar;
 import java.util.Date;
+import java.util.GregorianCalendar;
 
 /*
  * --EPOCH--
  * An epoch is a an absolute time reference. 
  * Most programming languages (e.g Java, JavaScript, Python) use the Unix epoch (Midnight 1 January 1970) 
- * when expressing a given timestamp as the number of milliseconds elapsed since a fixed point-in-time reference.
+ * when expressing a given Timestamp as the number of milliseconds elapsed since a fixed point-in-time reference.
  */
 
 public class EpochDemo {
 
 	public static void main(String[] args) {
 		
-		long epochTime =  145566778888L;  // Assume this is the epoch you received,
+		long epochTime =  1515996044000L;  // Assume this is the epoch you received,
 		                                  // if you receive a string, parse it as follows.
-		                                  // Long.parseLong("15567777888L");
+		                                  // Long.parseLong("1515996044000L");
 		
-		//If the epoch rececived is in seconds, coverts to milliseconds.
+		//If the epoch received is in seconds, converts to milliseconds (multiply by 1000L).
 		
 		Timestamp ts = new Timestamp(epochTime); // for creating Timestamp
 		Date d = new Date(epochTime);            // For creating Date
 		
-		
-		//check here with your valid values.
+		System.out.println("Date:"+ d);
+		System.out.println("Timestamp:" + ts);
 	}
 }
